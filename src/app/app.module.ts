64a04a09617313d782704ab404112df075e443d2
Orlando Brea <orlando.brea@gmail.com>
2021-01-22 10:45:14 -0300

    refactor(alumno): refactor de alumnos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu/menu.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule,
-  MatListModule, MatFormFieldModule, MatInputModule, MatCardModule,
-  MatRadioModule, MatSelectModule, MatCheckboxModule, MatTableModule,
-  MatMenuModule, MatDialogModule,
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatCheckboxModule,
+  MatTableModule,
+  MatMenuModule,
+  MatDialogModule,
 } from '@angular/material';
-import { AlumnoItemComponent } from './alumno-item/alumno-item.component';
-import { AlumnoListaComponent } from './alumno-lista/alumno-lista.component';
-import { AlumnoManagerComponent } from './alumno-manager/alumno-manager.component';
-import { FilterBoxComponent } from './filter-box/filter-box.component';
 import { FormsModule } from '@angular/forms';
-import { AlumnoEdicionComponent } from './alumno-edicion/alumno-edicion.component';
 import { CursoManagerComponent } from './curso-manager/curso-manager.component';
 import { AsistenciaManagerComponent } from './asistencia-manager/asistencia-manager.component';
 import { ConfirmDialogComponent } from './dialogs/confirm-dialog/confirm-dialog.component';
@@ -25,14 +31,9 @@ import { ConfirmDialogComponent } from './dialogs/confirm-dialog/confirm-dialog.
   declarations: [
     AppComponent,
     MenuComponent,
-    AlumnoItemComponent,
-    AlumnoListaComponent,
-    AlumnoManagerComponent,
-    FilterBoxComponent,
-    AlumnoEdicionComponent,
     CursoManagerComponent,
     AsistenciaManagerComponent,
-    ConfirmDialogComponent
+    ConfirmDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -54,19 +55,15 @@ import { ConfirmDialogComponent } from './dialogs/confirm-dialog/confirm-dialog.
     MatMenuModule,
     MatDialogModule,
     RouterModule.forRoot([
-      { path: 'alumnos' , component: AlumnoManagerComponent},
-      { path: 'alumnos/:operacion/:id' , component: AlumnoEdicionComponent},
-      { path: 'alumnos/:operacion' , component: AlumnoEdicionComponent},
-      { path: 'cursos' , component: CursoManagerComponent},
-      { path: 'asistencias' , component: AsistenciaManagerComponent},
-      { path: '',  redirectTo: '/alumnos', pathMatch: 'full' },
-      { path: '**',  redirectTo: '/alumnos', pathMatch: 'full' },
-    ])
+      { path: 'alumnos', loadChildren: () => import('./alumno/alumno.module').then((m) => m.AlumnoModule) },
+      { path: 'cursos', component: CursoManagerComponent },
+      { path: 'asistencias', component: AsistenciaManagerComponent },
+      { path: '', redirectTo: '/alumnos', pathMatch: 'full' },
+      { path: '**', redirectTo: '/alumnos', pathMatch: 'full' },
+    ]),
   ],
-  entryComponents: [
-    ConfirmDialogComponent
-  ],
+  entryComponents: [ConfirmDialogComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
